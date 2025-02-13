5ede687ff431dddb602e20ecf067ac755ebe03a8
Minh Nguyen <44615298+minh160302@users.noreply.github.com>
2025-02-12 19:38:43 -0800

    update test coverage file

--- CLI/coverage.sh
+++ CLI/coverage.sh
@@ -3,6 +3,6 @@
 # Clean test cache
 go clean -testcache
 # run tests and create a coverprofile
-go test ./... -coverprofile=../cover.out
+go test ./... -coverprofile=./cover.out
 # open the interactive UI to check the Coverage Repor
 go tool cover -html=./cover.out
