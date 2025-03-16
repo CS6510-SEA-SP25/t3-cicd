dfafa316e2e171da2d7e6f847510f3542483f941
Minh Nguyen <44615298+minh160302@users.noreply.github.com>
2025-03-15 17:29:02 -0700

    fix tests attempt 15

--- backend/containers/docker/api.go
+++ backend/containers/docker/api.go
@@ -217,6 +217,7 @@ func Execute(pipeline models.PipelineConfiguration, repository models.Repository
 	}
 	var pipelineReportId, err = pipelineService.CreatePipeline(pipelineReport)
 	if err != nil {
+		log.Println("debug 220");
 		return err
 	}
 
@@ -233,6 +234,7 @@ func Execute(pipeline models.PipelineConfiguration, repository models.Repository
 		}
 		var stageReportId, err = stageService.CreateStage(stageReport)
 		if err != nil {
+			log.Println("debug 237");
 			return err
 		}
 
@@ -325,6 +327,7 @@ func Execute(pipeline models.PipelineConfiguration, repository models.Repository
 					if err = pipelineService.UpdatePipelineStatusAndEndTime(pipelineReportId, models.FAILED); err != nil {
 						log.Printf("%v\n", err)
 					}
+					log.Println("debug 330");
 					return result.Err // Return the first error encountered
 				}
 			}
@@ -351,6 +354,7 @@ func Execute(pipeline models.PipelineConfiguration, repository models.Repository
 		pipelineService.UpdatePipelineStatusAndEndTime(pipelineReportId, models.SUCCESS)
 	}
 
+	log.Println("debug 357");
 	return nil
 }
 
